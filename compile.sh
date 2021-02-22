226c09f9f0d5f85cac1a251a40ac7f131e5f9f38
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-02-22 11:32:44 +0700

    BUILD V2

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ cd X01BD
 git clone --depth=1 https://github.com/STRIX-Project/STRIX-clang $clangDir clang
 git clone https://github.com/ZyCromerZ/aarch64-linux-android-4.9/ -b android-10.0.0_r47 --depth=1 gcc
 git clone https://github.com/ZyCromerZ/arm-linux-androideabi-4.9/ -b android-10.0.0_r47 --depth=1 gcc32
-git clone --depth=1 https://github.com/ramadhannangga/Anykernel3 AnyKernel3
+git clone --depth=1 https://github.com/ramadhannangga/Anykernel3 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%Y-%m-%d")
