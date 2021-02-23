898b5cae66b592210fe2a632a2033c50462dd288
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-02-23 19:38:11 +0700

    test build -v3

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone https://github.com/ramadhannangga/EPYC X01BD
+git clone https://github.com/ramadhannangga/X01BD X01BD
 cd X01BD
 git clone --depth=1 https://github.com/STRIX-Project/STRIX-clang $clangDir clang
 git clone https://github.com/ZyCromerZ/aarch64-linux-android-4.9/ -b android-10.0.0_r47 --depth=1 gcc
