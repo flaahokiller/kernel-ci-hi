2c36434ae64f6811e72af7fb987673eee99b06ba
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-02-24 20:49:27 +0700

    build branch main

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone https://github.com/ramadhannangga/EPYC -b X01BD-cam X01BD
+git clone https://github.com/ramadhannangga/EPYC -b X01BD-main X01BD
 cd X01BD
 git clone --depth=1 https://github.com/NusantaraDevs/clang $clangDir clang
 git clone https://github.com/ZyCromerZ/aarch64-linux-android-4.9/ -b android-10.0.0_r47 --depth=1 gcc
@@ -12,7 +12,7 @@ TANGGAL=$(date +"%Y-%m-%d")
 TGL=$(date +"%m%d")
 START=$(date +"%s")
 COMMIT=$(git log --pretty=format:'%h' -1)
-VARIANT="XR"
+VARIANT="BETA"
 COMPILE=NUSANTARA
 KERNELNAME="EPYC"
 KERNEL_DIR=$(pwd)
@@ -21,6 +21,7 @@ PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PA
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" 
 export ARCH=arm64
 export KERNELNAME=EPYC
+export LOCALVERSION=#NewSynergy
 export KBUILD_BUILD_USER="Naples"
 export KBUILD_BUILD_HOST=CircleCI-server
 export TOOLCHAIN=clang
