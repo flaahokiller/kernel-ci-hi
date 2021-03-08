9982e8e31395f75ff981a414f20ffd5f3d1bbf77
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-03-08 12:42:20 +0700

    build BETA-LAST

--- compile.sh
+++ compile.sh
@@ -12,7 +12,7 @@ TANGGAL=$(date +"%Y-%m-%d")
 TGL=$(date +"%m%d")
 START=$(date +"%s")
 COMMIT=$(git log --pretty=format:'%h' -1)
-VARIANT="BETA-5"
+VARIANT="BETA-LAST"
 COMPILE=NUSANTARA
 KERNELNAME="WonRebase"
 KERNEL_DIR=$(pwd)
