22a62a4a064d30b4417a9888226dc4fbbbed50d6
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-03-31 09:31:35 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 echo "Cloning dependencies"
 git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660 -b lineage-17.1-main X01BD
 cd X01BD
-git clone --depth=1 https://github.com/STRIX-Project/STRIX-clang $clangDir clang
+git clone --depth=1 https://github.com/GengKapak/GengKapak-clang $clangDir clang
 git clone https://github.com/ZyCromerZ/aarch64-linux-android-4.9/ -b android-10.0.0_r47 --depth=1 gcc
 git clone https://github.com/ZyCromerZ/arm-linux-androideabi-4.9/ -b android-10.0.0_r47 --depth=1 gcc32
 git clone --depth=1 https://github.com/ramadhannangga/Anykernel3 AnyKernel
@@ -12,7 +12,7 @@ TANGGAL=$(date +"%Y-%m-%d")
 TGL=$(date +"%m%d")
 START=$(date +"%s")
 COMMIT=$(git log --pretty=format:'%h' -1)
-VARIANT="BETA-3"
+VARIANT="BETA-4"
 COMPILE=CLANG
 KERNELNAME="LithoWonder"
 KERNEL_DIR=$(pwd)
