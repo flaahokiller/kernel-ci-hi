e31e5a13eff616539dfa37bfe61c5bc3f7ae0f4c
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-04-02 17:00:09 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -12,7 +12,7 @@ TANGGAL=$(date +"%Y-%m-%d")
 TGL=$(date +"%m%d")
 START=$(date +"%s")
 COMMIT=$(git log --pretty=format:'%h' -1)
-VARIANT="BETA-6"
+VARIANT="BETA-7"
 COMPILE=CLANG
 KERNELNAME="LithoWonder"
 KERNEL_DIR=$(pwd)
