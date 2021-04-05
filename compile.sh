e4d8e1b29807d45a361a9384466d654a01780918
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-04-05 13:28:28 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,9 +3,9 @@ echo "Cloning dependencies"
 git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660 -b lineage-17.1 X01BD
 cd X01BD
 git clone --depth=1 https://github.com/NusantaraDevs/clang $clangDir clang
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $gcc64Dir gcc
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm $gcc32Dir gcc32
-git clone --depth=1 https://github.com/ramadhannangga/Anykernel3 AnyKernel
+git clone --depth=1 https://github.com/ZyCromerZ/aarch64-linux-android-4.9/ -b android-10.0.0_r47 $gcc64Dir gcc
+git clone --depth=1 https://github.com/ZyCromerZ/arm-linux-androideabi-4.9/ -b android-10.0.0_r47 $gcc32Dir gcc32
+git clone --depth=1 https://github.com/ramadhannangga/Anykernel3-ASUS AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%Y-%m-%d")
