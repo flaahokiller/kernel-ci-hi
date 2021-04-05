656275b8e0bfac0b6c53698cd13f4762b9f6116a
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-04-05 13:15:30 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660 -b EAS X01BD
+git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660 -b lineage-17.1 X01BD
 cd X01BD
 git clone --depth=1 https://github.com/NusantaraDevs/clang $clangDir clang
-git clone https://github.com/ZyCromerZ/aarch64-linux-android-4.9/ -b android-10.0.0_r47 --depth=1 gcc
-git clone https://github.com/ZyCromerZ/arm-linux-androideabi-4.9/ -b android-10.0.0_r47 --depth=1 gcc32
+git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 gcc
+git clone https://github.com/mvaisakh/gcc-arm --depth=1 gcc32
 git clone --depth=1 https://github.com/ramadhannangga/Anykernel3 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
@@ -14,15 +14,14 @@ START=$(date +"%s")
 COMMIT=$(git log --pretty=format:'%h' -1)
 VARIANT="EAS"
 COMPILE=NUSANTARA
-KERNELNAME="REIGNITE"
+KERNELNAME="LithoWonder"
 KERNEL_DIR=$(pwd)
 VERSI=(""4.4.$(cat "$(pwd)/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"")
 PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}" 
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" 
 export ARCH=arm64
-export KERNELNAME=REIGNITE
-export LOCALVERSION=EAS
-export KBUILD_BUILD_USER="Naples"
+export KERNELNAME=LithoWonder
+export KBUILD_BUILD_USER="Wonder"
 export KBUILD_BUILD_HOST=CircleCI-server
 export TOOLCHAIN=clang
 export DEVICES=X01BD
@@ -66,8 +65,8 @@ function compile() {
                     ARCH=arm64 \
                     SUBARCH=arm64 \
                     CC=clang \
-                    CROSS_COMPILE=aarch64-linux-android- \
-                    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                    CROSS_COMPILE=aarch64-elf- \
+                    CROSS_COMPILE_ARM32=arm-eabi- \
                     AR=llvm-ar \
                     NM=llvm-nm \
                     OBJCOPY=llvm-objcopy \
