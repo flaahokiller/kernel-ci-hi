155ad1c09550f7ed47eea3b2ceba8d628a99293a
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-04-06 17:27:05 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660-2 -b lineage-17.1 X01BD
+git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660-2 -b EAS X01BD
 cd X01BD
 git clone --depth=1 https://github.com/ramadhannangga/Toolchain-Clang $clangDir clang
 git clone https://github.com/ramadhannangga/aarch64-linux-android-4.9 $gcc64Dir gcc
