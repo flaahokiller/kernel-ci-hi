001cc7f243deb235846758ae2aa2642be298bc6f
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-04-08 20:55:41 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone https://github.com/ramadhannangga/android_kernel_asus_sdm660-2 -b EAS X01BD
+git clone https://github.com/ramadhannangga/KERNEL_X01BD -b HMP X01BD
 cd X01BD
 git clone --depth=1 https://github.com/ramadhannangga/Toolchain-Clang $clangDir clang
 git clone https://github.com/ramadhannangga/aarch64-linux-android-4.9 $gcc64Dir gcc
 git clone https://github.com/ramadhannangga/arm-linux-androideabi-4.9 $gcc32Dir gcc32
-git clone https://github.com/ramadhannangga/Anykernel3-ASUS -b EAS AnyKernel
+git clone https://github.com/ramadhannangga/Anykernel3 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%Y-%m-%d")
@@ -14,13 +14,13 @@ START=$(date +"%s")
 COMMIT=$(git log --pretty=format:'%h' -1)
 FOR="XR"
 COMPILE=CLANG
-KERNELNAME="LithoWonder-EAS"
+KERNELNAME="LithoWonder-HMP"
 KERNEL_DIR=$(pwd)
 VERSI=(""4.4.$(cat "$(pwd)/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"")
 PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}" 
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" 
 export ARCH=arm64
-export KERNELNAME=LithoWonder-EAS
+export KERNELNAME=LithoWonder-HMP
 export KBUILD_BUILD_USER="ramadhannangga"
 export KBUILD_BUILD_HOST=localhost
 export TOOLCHAIN=clang
